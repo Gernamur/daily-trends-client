256ea59f10583ca53022cb453556266b2f274478
Germán Navarro <german.navarro@outlook.es>
2021-10-06 11:25:09 +0200

    added main functionality. Feeds front-page list

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <h1>Daily Trends</h1>
+    <main-component />
+  </div>
+</template>
+
+<script>
+import MainComponent from "./components/MainComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainComponent,
+  },
+};
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.app > h1 {
+  font-family: Helvetica;
+  font-style: italic;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
